f2ef4986ee8daca3955d2b984dbf411ba1b1ced7
Annie <annielin28815@gmail.com>
2023-05-05 23:19:34 +0800

    local storage

--- day15/main.js
+++ day15/main.js
@@ -3,8 +3,11 @@ const itemsList = document.querySelector('.plates');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
 function addItem(e) {
+  // 避免每次submit都會重整網頁
   e.preventDefault();
+  // 利用再次 querySelector 來選取 form 中的 input 欄位值
   const text = (this.querySelector('[name=item]')).value;
+  // 宣告新增要存入的物件，是輸入的文字與是否勾選的狀態(done)
   const item = {
     text,
     done: false
@@ -13,10 +16,13 @@ function addItem(e) {
   items.push(item);
   populateList(items, itemsList);
   localStorage.setItem('items', JSON.stringify(items));
+  // 清空輸入欄位
   this.reset();
 }
 
+// 在 function 中的參數直接設定參數預設值
 function populateList(plates = [], platesList) {
+  // 使用 map 搭配 join 來組成字串，並顯示在 html 的清單 ul 中
   platesList.innerHTML = plates.map((plate, i) => {
     return `
     <li>
@@ -28,14 +34,22 @@ function populateList(plates = [], platesList) {
 }
 
 function toggleDone(e) {
-  if (!e.target.matches('input')) return; // skip this unless it's an input
+  /* skip this unless it's an input
+  * 偵測進來的點擊是input(checkbox)才動作
+  */
+  if (!e.target.matches('input')) return;
+  // 取得checkbox的data-index值
   const el = e.target;
   const index = el.dataset.index;
+  // 利用！來使done的狀態在true/false間切換
   items[index].done = !items[index].done;
+  // 將更新後的狀態寫入localStorage中
   localStorage.setItem('items', JSON.stringify(items));
+  // 更新列表
   populateList(items, itemsList);
 }
 
+// 監聽 submit 按鈕
 addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone);
 
