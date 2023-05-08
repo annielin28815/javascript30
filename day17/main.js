7567274f94d5f9d327196711d33f723d2a1fb573
Annie <annielin28815@gmail.com>
2023-05-08 23:59:42 +0800

    replace

--- day17/main.js
+++ day17/main.js
@@ -1,6 +1,7 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 function strip(bandName) {
+  // 用 replace 搭配正規表示式
   return bandName.replace(/^(a |the |an )/i, '').trim();
 }
 
